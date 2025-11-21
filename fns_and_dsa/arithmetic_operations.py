ffee2c68296888257774bac37ca03d02bf79b646
KK-Bismark <kbismarkkwaku@gmail.com>
2025-11-21 22:13:41 +0000

    Solutions for task 0 that deals with implementing the function for basic arithmetic calculations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,2 +1,18 @@
 # A python program that has a function that performs basic arithmetic
 # operations.
+
+def perform_operation(num1, num2, operation):
+    # checking to see which operation to use
+    match operation:
+        case "add":
+            return (num1 + num2)
+        case "subtract":
+            return (num1 - num2)
+        case "multiply":
+            return (num1 * num2)
+        case "divide":
+            if num2 == 0:
+                result = "The first number isn't divisible by 0"
+                return (result)
+            else:
+                return (num1 / num2)
