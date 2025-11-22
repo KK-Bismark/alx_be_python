ac5d3003a5cbd2594c6b3996b567f6f3fd9f977e
KK-Bismark <kbismarkkwaku@gmail.com>
2025-11-22 11:48:41 +0000

    Learnt how to handle datetime in python.

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,27 @@
+# A script that performs specified operations with dates and times.
+
+
+import datetime
+
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {current_date}")
+
+
+def calculate_future_date():
+    n_days = int(input("Enter the number of days to add to the current date: "))
+    today = datetime.date.today()
+    future_date = today + datetime.timedelta(days=n_days)
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
+
+
+def main():
+    display_current_datetime()
+    calculate_future_date()
+
+
+
+if __name__ == "__main__":
+    main()
