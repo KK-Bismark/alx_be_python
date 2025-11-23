0961371b50306ff332e7018e0cef763a0309a8ea
KK-Bismark <kbismarkkwaku@gmail.com>
2025-11-23 15:01:29 +0000

    Solved the task on converting from one unit of temperature to the other.

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,44 @@
+# A python program that converts temperatures between Celsius and Fahrenheit, 
+# using global variables to store conversion factors.
+
+
+# Global variables
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+
+# A function to convert from fahrenheit to celcius
+def convert_to_celsius(fahrenheit):
+    temp_celcius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temp_celcius
+
+
+# A function to convert from celcius to fahrenheit
+def convert_to_fahrenheit(celsius):
+    temp_fahrenheit = celcius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return temp_fahrenheit
+
+
+# Main Function
+def main():
+    # prompt for user values
+    user_temp = float(input("Enter a temperature: "))
+    temp_unit = input("Is it in celsius or fahrenheit? (C/F) ").upper()
+
+    # check to see which function to call
+    if temp_unit == 'C':
+        new_temp = convert_to_fahrenheit(user_temp)
+        new_temp_unit = 'F'
+    elif temp_unit == 'F':
+        new_temp = convert_to_celsius(user_temp)
+        new_temp_unit = 'C'
+    else:
+        print("You selected a wrong unit.")
+
+    # print the new temperature in the converted unit
+    print(f"{user_temp}{temp_unit} is {new_temp}{new_temp_unit}")
+
+
+
+if __name__ == "__main__":
+    main()
