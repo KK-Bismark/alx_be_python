849d0b322c4591e59bdb01a55c1aa231c11b4795
KK-Bismark <kbismarkkwaku@gmail.com>
2025-11-29 17:31:34 +0000

    Solution to task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+# BankAccount class that encaspulated bannking operations.
+
+
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    # function that handles depositing of money into the bank account.
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    # function that handles withdrawal of money
+    def withdraw(self, amount):
+        self.account_balance -= amount
+
+    # function that handles the displaying of the account balance.
+    def display_balance(self):
+        return self.account_balance
