640924c685139d30c47ae395f853db7607f3e729
KK-Bismark <kbismarkkwaku@gmail.com>
2025-11-29 17:54:34 +0000

    Display my account balance function implemented

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
 
     # function that handles the displaying of the account balance.
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
