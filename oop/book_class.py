1c58bf7031afa5af845e001c17a3c70ec768dab9
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-05 13:48:15 +0000

    Solution for task0.

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+# A python program that uses specific magic methods to enhance its
+# functionality. This class will model a book with attributes for
+# the title, author, and publication year.
+
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
