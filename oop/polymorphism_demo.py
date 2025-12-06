c290b9ca6a78a2ea7ce4a25fff5ad542902a05df
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-06 09:32:55 +0000

    Solution for task2

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,34 @@
+# A python script that a has base class Shape with a method area() and derived
+# classes Rectangle and Circle, each overriding the area() method to calculate
+# their respective areas.
+
+import math
+
+
+class Shape:
+    def area(self):
+        """
+            simply raises a NotImplementedError, indicating that derived 
+            classes need to override this method.
+            """
+        raise NotImplementedError('Derived class need to override this method')
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """Calculates the area of a circle."""
+        solution = math.pi * self.radius * self.radius
+        return solution
